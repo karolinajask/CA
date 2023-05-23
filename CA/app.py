5df9697d8a3d29b9b77ac8ed86618c8abc06cddc
Karolina Jaskolska <10540041@mydbs.ie>
2023-05-23 21:01:32 +0100

    update.html etc

--- CA/app.py
+++ CA/app.py
@@ -29,7 +29,7 @@ def index():
     conn = mysql.connection
     cur = conn.cursor(MySQLdb.cursors.DictCursor) 
     #executing query
-    cur.execute("select AdDate, Wanted, Price, Used, CarModel, CarColour from Ad")
+    cur.execute("select AdId,AdDate, Wanted, Price, Used, CarModel, CarColour from Ad")
     #fetching all records from database
     data=cur.fetchall()
     #returning back to projectlist.html with all records from MySQL which are stored in variable data
@@ -115,9 +115,6 @@ def login():
 
     return render_template('auth/login.html')
 
-@app.route("/home")
-def home():
-    return render_template('auth/home.html')
 
 @app.before_request
 def load_logged_in_user():
@@ -148,6 +145,19 @@ def login_required(view):
 
     return wrapped_view
 
+@app.route("/home")
+def home():
+    user_id = session.get('user_id')
+    #creating variable for connection
+    conn = mysql.connection
+    cur = conn.cursor(MySQLdb.cursors.DictCursor) 
+    #executing query
+    cur.execute("select AdId, AdDate, Wanted, Price, Used, CarModel, CarColour from Ad where PosterID = %s" , (user_id,) )
+    #fetching all records from database
+    data=cur.fetchall()
+    #returning back to projectlist.html with all records from MySQL which are stored in variable data
+    return render_template('auth/home.html',data=data)
+
 @app.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
@@ -160,8 +170,7 @@ def create():
         price = request.form['price']
         conn = mysql.connection
         cur = conn.cursor()
-        error = None
-                
+        error = None                
 
         cur.execute("SELECT CarID FROM Ad WHERE CarID = %s", [carid])
         x = cur.fetchone()
@@ -173,23 +182,17 @@ def create():
             cur.execute("SELECT RoleId FROM User WHERE UserEmail = %s", [user_id])
             u = cur.fetchone()
             usrole = str(u[0])
-            print(usrole)
 
-            try:
-                           
+            try:                          
 
                 if usrole == "seller":
-                    print(usrole)
                     cur.execute(                        
                         "INSERT INTO Ad (Wanted, CarID, PosterID, Price,Used, CarModel,CarColour) VALUES (%s, %s, %s, %s, %s, %s, %s)",
                         ("n", carid, user_id, price, carused, carmodel, carcolour),
                     )
                 conn.commit()
-                
-
 
                 if usrole == "buyer":
-                    print(usrole)
                     cur.execute(                        
                         "INSERT INTO Ad (Wanted, CarID, PosterID, Price,Used, CarModel,CarColour) VALUES (%s, %s, %s, %s, %s, %s, %s)",
                         ("y", carid, user_id, price, carused, carmodel, carcolour),
@@ -202,60 +205,28 @@ def create():
 
     return render_template('auth/create.html')
 
+#################################################################
 
-#def get_post(id, check_author=True):
- #   post = get_db().execute(
-  #      'SELECT p.id, title, body, created, author_id, username'
-   #     ' FROM post p JOIN user u ON p.author_id = u.id'
-    #    ' WHERE p.id = ?',
-     #   (id,)
-   # ).fetchone()
+@app.route('/update', methods=('GET', 'POST'))
+@login_required
+def update():
+    user_id = session.get('user_id')
+    #creating variable for connection
+    conn = mysql.connection
+    cur = conn.cursor(MySQLdb.cursors.DictCursor) 
+    #executing query
+    cur.execute("select AdId, AdDate, Wanted, Price, Used, CarModel, CarColour from Ad where PosterID = %s" , (user_id,) )
+    #fetching all records from database
+    data=cur.fetchall()
+    #returning back to projectlist.html with all records from MySQL which are stored in variable data
 
-    #if post is None:
-     #   abort(404, f"Post id {id} doesn't exist.")
+    return render_template('auth/update.html',data=data)
+#################################################################
 
-    #if check_author and post['author_id'] != g.user['id']:
-     #   abort(403)
-
-#    return post
-
-
-#@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-#@login_required
-#def update(id):
- #   post = get_post(id)
-
-  #  if request.method == 'POST':
-   #     title = request.form['title']
-    #    body = request.form['body']
-     #   error = None
-     #
-       # if not title:
-#            error = 'Title is required.'
-
- #       if error is not None:
-  #          flash(error)
-   #     else:
-    #        db = get_db()
-     #       db.execute(
-      #          'UPDATE post SET title = ?, body = ?'
-       #         ' WHERE id = ?',
-        #        (title, body, id)
-         #   )
-           # db.commit()
-          #  return redirect(url_for('blog.index'))
-
-   # return render_template('blog/update.html', post=post)
-
-
-#@bp.route('/<int:id>/delete', methods=('POST',))
-#@login_required
-#def delete(id):
-#    get_post(id)
- #   db = get_db()
-  #  db.execute('DELETE FROM post WHERE id = ?', (id,))
-   # db.commit()
-    #return redirect(url_for('blog.index'))
+@app.route('/delete', methods=('POST',))
+@login_required
+def delete():
+     return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
